40cefbef108fc9b20dd26278cc3e4cc80ce78ae8
Wei Zeng <gzzengwei@gmail.com>
2021-11-15 00:30:56 +1100

    Use BroadcastDispatcher with queue

--- lib/job_demo/producer.ex
+++ lib/job_demo/producer.ex
@@ -2,23 +2,38 @@ defmodule JobDemo.Producer do
   use GenStage
 
   def start_link(args) do
-    GenStage.start_link(JobDemo.Producer, args, name: __MODULE__)
+    GenStage.start_link(__MODULE__, args, name: __MODULE__)
   end
 
-  def sync_notify(event, timeout \\ 5000) do
+  def sync_notify(event, timeout \\ 1_000 * 30) do
     GenStage.call(__MODULE__, {:notify, event}, timeout)
   end
 
   def init(state) do
-    {:producer, state, dispatcher: GenStage.BroadcastDispatcher}
+    {:producer, {:queue.new(), 0}, dispatcher: GenStage.BroadcastDispatcher}
   end
 
   def handle_demand(_, state) do
     {:noreply, [], state}
   end
 
-  def handle_call({:notify, event}, _from, state) do
-    # Dispatch immediately
-    {:reply, :ok, [event], state}
+  def handle_call({:notify, event}, from, {queue, pending_demand}) do
+    queue = :queue.in({from, event}, queue)
+    dispatch_events(queue, pending_demand, [])
+  end
+
+  def handle_demand(incoming_demand, {queue, pending_demand}) do
+    dispatch_events(queue, incoming_demand + pending_demand, [])
+  end
+
+  defp dispatch_events(queue, demand, events) do
+    case :queue.out(queue) do
+      {{:value, {from, event}}, queue} ->
+        GenStage.reply(from, :ok)
+        dispatch_events(queue, demand - 1, [event | events])
+
+      {:empty, queue} ->
+        {:noreply, Enum.reverse(events), {queue, demand}}
+    end
   end
 end
